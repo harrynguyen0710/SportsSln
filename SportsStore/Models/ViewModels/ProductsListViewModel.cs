da1d9ad202c16f8f6b6dc851065f83a133e1614f
harrynguyen0710 <nnhoang0710@gmail.com>
2023-11-23 19:45:31 +0700

    Add project files.

---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+            = Enumerable.Empty<Product>();
+        public PagingInfo PagingInfo { get; set; } = new();
+    }
+}
